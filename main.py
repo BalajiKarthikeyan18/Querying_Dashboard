5572b03104ec17e8db5ed634a2ed2121fe475fae
ash3173 <em2ashwinrajesh@gmail.com>
2024-11-26 09:59:22 +0530

    Trial

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def natural_sort(files):
 
 # Streamlit app starts here
 def main():
-    st.title("Temporal Graph Dashboard")
+    st.title("Temporal Graph Dashboard Trial")
 
     # Specify the directory containing JSON files
     data_directory = "D:/LAM/End Card/EndCard/size_1000/"
